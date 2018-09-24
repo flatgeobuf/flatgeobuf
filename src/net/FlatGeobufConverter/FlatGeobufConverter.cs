c8b352c8d6b8ee90ec14c64f52bd273e175948f1
Björn Harrtell <bjorn@septima.dk>
2018-09-24 16:14:32 +0200

    Minimal conversion console app

---
+++ src/net/FlatGeobufConverter/FlatGeobufConverter.cs
@@ -0,0 +1,23 @@
+﻿using NetTopologySuite.Features;
+using NetTopologySuite.IO;
+using System.IO;
+
+namespace FlatGeobuf
+{
+    public class FlatGeobufConverter
+    {
+        public static void Main(string[] args)
+        {
+            var inFile = args[0];
+            var outFile = args[1];
+
+            var geojson = File.ReadAllText(inFile);
+
+            var reader = new GeoJsonReader();
+            var fc = reader.Read<FeatureCollection>(geojson);
+            var bytes = FlatGeobuf.NTS.FeatureCollectionConversions.ToFlatGeobuf(fc);
+
+            File.WriteAllBytes(outFile, bytes);
+        }
+    }
+}
