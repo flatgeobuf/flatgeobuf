5bc6bb11f31bb0f23f7c42571b3aa2f945196c57
Björn Harrtell <bjorn@wololo.org>
2018-10-10 14:22:37 +0200

    Upgrade C++ flatbuffers to 1.10.0

--- src/cpp/fetch.sh
+++ src/cpp/fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -L https://github.com/google/flatbuffers/archive/master.tar.gz | tar xz --strip-components=2 flatbuffers-master/include
+curl -L https://github.com/google/flatbuffers/archive/v1.10.0.tar.gz | tar xz --strip-components=2 flatbuffers-1.10.0/include
 
