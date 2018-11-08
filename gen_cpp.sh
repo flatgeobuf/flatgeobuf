4787754a670df2786adcdf50be9ce6d7f6aa6bdc
Björn Harrtell <bjorn@wololo.org>
2018-11-08 12:51:23 +0100

    Switch to per buffer header

--- gen_cpp.sh
+++ gen_cpp.sh
@@ -2,4 +2,4 @@
 #./flatc --ts --gen-all -o src column.fbs
 #./flatc --ts --no-ts-reexport -o src geometry.fbs
 #./flatc --ts --no-ts-reexport -o src feature.fbs
-./flatc --cpp --gen-all --scoped-enums -o src/cpp src/fbs/flatgeobuf.fbs
+./flatc --cpp --scoped-enums -o src/cpp src/fbs/*.fbs
