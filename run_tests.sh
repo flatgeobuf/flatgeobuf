c13846850be24193d918ca74030ec8e72001d7ec
Björn Harrtell <bjorn@septima.dk>
2019-05-06 22:15:45 +0200

    Add tsconfig for test runs

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e '\033[1;33m--- Running TypeScript tests ---'
-mocha -r ts-node/register -r esm src/**/*.spec.ts
+TS_NODE_PROJECT=tsconfig.test.json mocha -r ts-node/register -r esm src/**/*.spec.ts
 
 echo -e '\033[1;33m--- Compiling C++ tests ---'
 clang++ -std=c++14 -Wall -Wfloat-conversion -Wmissing-declarations -g -o ./testcpp -Isrc/cpp/include src/cpp/packedrtree.cpp src/cpp/test/run_tests.cpp
