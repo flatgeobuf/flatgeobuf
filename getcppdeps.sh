a18de80e0b24f6768170171bc54d956ded7bcc33
Björn Harrtell <bjorn@septima.dk>
2019-05-13 18:27:29 +0200

    Also fetch catch

--- getcppdeps.sh
+++ getcppdeps.sh
@@ -9,5 +9,4 @@ curl -L https://github.com/mapbox/variant/archive/v1.1.4.tar.gz | \
     tar xz -C src/cpp/include --strip-components=2 variant-1.1.4/include
 curl -L https://github.com/Tencent/rapidjson/archive/v1.1.0.tar.gz | \
     tar xz -C src/cpp/include --strip-components=2 rapidjson-1.1.0/include
-
-
+curl -L https://github.com/catchorg/Catch2/releases/download/v2.7.2/catch.hpp -o src/cpp/test/catch.hpp
--- run_tests.sh
+++ run_tests.sh
@@ -10,4 +10,4 @@ echo -e '\033[1;33m--- Compiling C++ tests ---'
 clang++ -std=c++14 -Wall -Wfloat-conversion -Wmissing-declarations -g -o ./testcpp -Isrc/cpp/include src/cpp/packedrtree.cpp src/cpp/test/run_tests.cpp
 
 echo -e '\033[1;33m--- Running C++ tests ---'
-./testcpp
+./testcpp -d yes
