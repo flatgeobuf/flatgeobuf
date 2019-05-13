09f8a5e9e573810b85a2ef8142164521f45e3832
Björn Harrtell <bjorn@septima.dk>
2019-05-13 18:16:52 +0200

    Remove cpp deps and automate getting them

---
+++ getcppdeps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+curl -L https://github.com/google/flatbuffers/archive/v1.11.0.tar.gz | \
+    tar xz -C src/cpp/include --strip-components=2 flatbuffers-1.11.0/include
+curl -L https://github.com/mapbox/geojson-cpp/archive/v0.4.3.tar.gz | \
+    tar xz -C src/cpp/include --strip-components=2 geojson-cpp-0.4.3/include
+curl -L https://github.com/mapbox/geometry.hpp/archive/v1.0.0.tar.gz | \
+    tar xz -C src/cpp/include --strip-components=2 geometry.hpp-1.0.0/include
+curl -L https://github.com/mapbox/variant/archive/v1.1.4.tar.gz | \
+    tar xz -C src/cpp/include --strip-components=2 variant-1.1.4/include
+curl -L https://github.com/Tencent/rapidjson/archive/v1.1.0.tar.gz | \
+    tar xz -C src/cpp/include --strip-components=2 rapidjson-1.1.0/include
+
+
