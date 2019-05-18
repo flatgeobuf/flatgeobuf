bf5fca1275c4b7a114f58e64ff91ccce033233d0
Björn Harrtell <bjorn@wololo.org>
2019-05-18 17:49:01 +0200

    Properties read/write C# code

--- src/net/FlatGeobuf/NTS/FeatureConversions.cs
+++ src/net/FlatGeobuf/NTS/FeatureConversions.cs
@@ -6,37 +6,50 @@ using FlatBuffers;
 
 namespace FlatGeobuf.NTS
 {
-    public static class FeatureConversions {
-        public static byte[] ToByteBuffer(IFeature feature, GeometryType geometryType, byte dimensions, IList<ColumnMeta> columns) {
+    public static class FeatureConversions
+    {
+        public static byte[] ToByteBuffer(IFeature feature, GeometryType geometryType, byte dimensions, IList<ColumnMeta> columns)
+        {
             // TODO: size might not be enough, need to be adaptive
             var builder = new FlatBufferBuilder(1024);
 
             var go = GeometryConversions.BuildGeometry(builder, feature.Geometry, geometryType, dimensions);
 
-            
-            /*VectorOffset? valuesOffset = null;
-            if (feature.Attributes != null && feature.Attributes.Count > 0 && columns != null) {
-                var valueOffsets = new List<Offset<Value>>();
-
-                foreach (var column in columns) {
-                    if (feature.Attributes.Exists(column.Name)) {
-                        ushort columnIndex = (ushort) columns.IndexOf(column);
+            ByteBuffer bb = null;
+            int offset = 0;
+            if (feature.Attributes != null && feature.Attributes.Count > 0 && columns != null)
+            {
+                bb = new ByteBuffer(1024 * 1024);
+                foreach (var column in columns)
+                {
+                    if (feature.Attributes.Exists(column.Name))
+                    {
+                        ushort columnIndex = (ushort)columns.IndexOf(column);
+                        bb.PutUshort(offset, columnIndex);
+                        offset += 2;
                         var value = feature.Attributes[column.Name];
-                        switch(value) {
+                        switch (value)
+                        {
                             case bool v:
-                                valueOffsets.Add(Value.CreateValue(builder, columnIndex, bool_value: v));
+                                bb.PutByte(offset, v ? (byte)1 : (byte)0);
+                                offset += 1;
                                 break;
                             case int v:
-                                valueOffsets.Add(Value.CreateValue(builder, columnIndex, int_value: v));
+                                bb.PutInt(offset, v);
+                                offset += 4;
                                 break;
                             case long v:
-                                valueOffsets.Add(Value.CreateValue(builder, columnIndex, long_value: v));
+                                bb.PutLong(offset, v);
+                                offset += 8;
                                 break;
                             case double v:
-                                valueOffsets.Add(Value.CreateValue(builder, columnIndex, double_value: v));
+                                bb.PutDouble(offset, v);
+                                offset += 8;
                                 break;
                             case string v:
-                                valueOffsets.Add(Value.CreateValue(builder, columnIndex, string_valueOffset: builder.CreateString(v)));
+                                bb.PutInt(offset, v.Length);
+                                offset += 4;
+                                bb.PutStringUTF8(offset, v);
                                 break;
                             case null:
                                 break;
@@ -44,8 +57,12 @@ namespace FlatGeobuf.NTS
                         }
                     }
                 }
-                valuesOffset = Feature.CreateValuesVector(builder, valueOffsets.ToArray());
-            }*/
+            }
+
+
+            VectorOffset? propertiesOffset = null;
+            if (bb != null)
+                propertiesOffset = Feature.CreatePropertiesVector(builder, bb.ToArray(0, offset));
 
             Feature.StartFeature(builder);
             Feature.AddCoords(builder, go.coordsOffset.Value);
@@ -55,19 +72,19 @@ namespace FlatGeobuf.NTS
                 Feature.AddRingLengths(builder, go.ringLengthsOffset.Value);
             if (go.ringCountsOffset.HasValue)
                 Feature.AddRingCounts(builder, go.ringCountsOffset.Value);
-            
-            //if (valuesOffset.HasValue)
-            //    Feature.AddValues(builder, valuesOffset.Value);
-            var offset = Feature.EndFeature(builder);
+            if (propertiesOffset.HasValue)
+                Feature.AddProperties(builder, propertiesOffset.Value);
+            var featureOffset = Feature.EndFeature(builder);
 
-            builder.FinishSizePrefixed(offset.Value);
+            builder.FinishSizePrefixed(featureOffset.Value);
 
             var bytes = builder.DataBuffer.ToSizedArray();
 
             return bytes;
         }
 
-        public static IFeature FromByteBuffer(ByteBuffer bb, Header header) {
+        public static IFeature FromByteBuffer(ByteBuffer bb, Header header)
+        {
             IList<Column> columns = null;
             if (header.ColumnsLength > 0)
             {
@@ -82,32 +99,47 @@ namespace FlatGeobuf.NTS
             var feature = Feature.GetRootAsFeature(bb);
             IAttributesTable attributesTable = null;
 
-            //if (feature.ValuesLength > 0)
-            //    attributesTable = new AttributesTable();
-
-            /*for (int i = 0; i < feature.ValuesLength; i++)
+            var propertiesArray = feature.GetPropertiesArray();
+            int offset = 0;
+            if (propertiesArray != null && propertiesArray.Length > 0)
             {
-                var value = feature.Values(i).Value;
-                var column = columns[value.ColumnIndex];
-                switch (column.Type) {
-                    case ColumnType.Bool:
-                        attributesTable.AddAttribute(column.Name, value.BoolValue);
-                        break;
-                    case ColumnType.Int:
-                        attributesTable.AddAttribute(column.Name, value.IntValue);
-                        break;
-                    case ColumnType.Long:
-                        attributesTable.AddAttribute(column.Name, value.LongValue);
-                        break;
-                    case ColumnType.Double:
-                        attributesTable.AddAttribute(column.Name, value.DoubleValue);
-                        break;
-                    case ColumnType.String:
-                        attributesTable.AddAttribute(column.Name, value.StringValue);
-                        break;
-                    default: throw new ApplicationException("Unknown type");
+                bb = new ByteBuffer(feature.GetPropertiesArray());
+                attributesTable = new AttributesTable();
+                while (offset < bb.Length)
+                {
+                    ushort i = bb.GetUshort(offset);
+                    offset += 2;
+                    var column = columns[i];
+                    var type = column.Type;
+                    var name = column.Name;
+                    switch (type)
+                    {
+                        case ColumnType.Bool:
+                            attributesTable.AddAttribute(name, bb.Get(offset) > 0 ? true : false);
+                            offset += 1;
+                            break;
+                        case ColumnType.Int:
+                            attributesTable.AddAttribute(name, bb.GetInt(offset));
+                            offset += 4;
+                            break;
+                        case ColumnType.Long:
+                            attributesTable.AddAttribute(name, bb.GetLong(offset));
+                            offset += 8;
+                            break;
+                        case ColumnType.Double:
+                            attributesTable.AddAttribute(name, bb.GetDouble(offset));
+                            offset += 8;
+                            break;
+                        case ColumnType.String:
+                            int len = bb.GetInt(offset);
+                            offset += 4;
+                            attributesTable.AddAttribute(name, bb.GetStringUTF8(offset, len));
+                            offset += len;
+                            break;
+                        default: throw new ApplicationException("Unknown type");
+                    }
                 }
-            }*/
+            }
 
             var geometry = GeometryConversions.FromFlatbuf(feature, header.GeometryType, header.Dimensions);
             var f = new NetTopologySuite.Features.Feature(geometry, attributesTable);
