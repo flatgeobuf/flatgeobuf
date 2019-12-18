2ff5fb29c95d58be5a77ed99aa0c2a6d6ebaa41f
Björn Harrtell <bjorn@septima.dk>
2019-12-18 17:31:13 +0100

    Upgrade deps

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,4 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve'
+import resolve from '@rollup/plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
