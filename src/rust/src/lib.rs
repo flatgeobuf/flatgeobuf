ca13ce865db9e5f0e15ec29a58ffce27d3804948
Pirmin Kalberer <pka@sourcepole.ch>
2020-02-29 23:56:13 +0100

    Add flatc types for Rust

---
+++ src/rust/src/lib.rs
@@ -0,0 +1,12 @@
+#[allow(dead_code, unused_imports, non_snake_case)]
+mod feature_generated;
+#[allow(dead_code, unused_imports, non_snake_case)]
+mod header_generated;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
