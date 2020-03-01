c52be9965ac8910747b147e5d34ce5cc9122d452
Pirmin Kalberer <pka@sourcepole.ch>
2020-03-01 10:21:26 +0100

    Finish Rust impl of C++ point test

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -13,38 +13,46 @@ mod tests {
     #[test]
     /// Verify size of feature/geometry with point
     fn point() {
-        // int size;
-
-        // FlatBufferBuilder fbb1;
-        // FlatBufferBuilder fbb2;
-        // FlatBufferBuilder fbb3;
-
-        let mut fbb1 = flatbuffers::FlatBufferBuilder::new();
-
-        // std::vector<double> xy;
-        // xy.push_back(0);
-        // xy.push_back(0);
-
-        // auto g1 = CreateGeometryDirect(fbb1, nullptr, nullptr);
-        // fbb1.Finish(g1);
-        // size = fbb1.GetSize();
-        // REQUIRE(size == 12);
-
-        let g1 = Geometry::create(&mut fbb1, &GeometryArgs::default());
-        fbb1.finish(g1, None);
-        let buf = fbb1.finished_data();
+        let mut fbb = flatbuffers::FlatBufferBuilder::new();
+        let g1 = Geometry::create(&mut fbb, &Default::default());
+        fbb.finish(g1, None);
+        let buf = fbb.finished_data();
         let size = buf.len();
         assert_eq!(size, 12);
 
-        // auto g2 = CreateGeometryDirect(fbb2, nullptr, &xy);
-        // fbb2.Finish(g2);
-        // size = fbb2.GetSize();
-        // REQUIRE(size == 40);
+        let mut fbb = flatbuffers::FlatBufferBuilder::new();
+        let xy = fbb.create_vector(&[0.0, 0.0]);
+        let g2 = Geometry::create(
+            &mut fbb,
+            &GeometryArgs {
+                xy: Some(xy),
+                ..Default::default()
+            },
+        );
+        fbb.finish(g2, None);
+        let buf = fbb.finished_data();
+        let size = buf.len();
+        assert_eq!(size, 40);
 
-        // auto g3 = CreateGeometryDirect(fbb3, nullptr, &xy);
-        // auto f = CreateFeatureDirect(fbb3, g3);
-        // fbb3.Finish(f);
-        // size = fbb3.GetSize();
-        // REQUIRE(size == 56);
+        let mut fbb = flatbuffers::FlatBufferBuilder::new();
+        let xy = fbb.create_vector(&[0.0, 0.0]);
+        let g3 = Geometry::create(
+            &mut fbb,
+            &GeometryArgs {
+                xy: Some(xy),
+                ..Default::default()
+            },
+        );
+        let f = Feature::create(
+            &mut fbb,
+            &FeatureArgs {
+                geometry: Some(g3),
+                ..Default::default()
+            },
+        );
+        fbb.finish(f, None);
+        let buf = fbb.finished_data();
+        let size = buf.len();
+        assert_eq!(size, 56);
     }
 }
