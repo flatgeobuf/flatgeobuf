c5d36e235f6120748970efb347270fb59b1a6bfa
Pirmin Kalberer <pka@sourcepole.ch>
2020-03-07 19:51:46 +0100

    Implement geometry visitor

--- src/rust/src/reader.rs
+++ src/rust/src/reader.rs
@@ -36,6 +36,7 @@ impl<R: Read + Seek> Reader<R> {
     }
     pub fn select_all(&mut self) -> std::result::Result<(), std::io::Error> {
         let header = get_root_as_header(&self.header_buf[..]);
+        // Skip index
         let index_size = packed_rtree_size(header.features_count(), header.index_node_size());
         self.reader.seek(SeekFrom::Current(index_size as i64))?;
         Ok(())
@@ -49,6 +50,141 @@ impl<R: Read + Seek> Reader<R> {
         self.reader.read_exact(&mut self.feature_buf)?;
         let feature = get_root_as_feature(&self.feature_buf[..]);
         Ok(feature)
+    }
+}
+
+pub trait GeomVisitor {
+    fn pointxy(&mut self, _x: f64, _y: f64) {}
+    fn multipoint_begin(&mut self, _size: usize) {}
+    fn multipoint_end(&mut self) {}
+    fn line_begin(&mut self, _size: usize) {}
+    fn line_end(&mut self) {}
+    fn multiline_begin(&mut self, _size: usize) {}
+    fn multiline_end(&mut self) {}
+    fn ring_begin(&mut self, _size: usize) {}
+    fn ring_end(&mut self) {}
+    fn poly_begin(&mut self, _size: usize) {}
+    fn poly_end(&mut self) {}
+    fn multipoly_begin(&mut self, _size: usize) {}
+    fn multipoly_end(&mut self) {}
+}
+
+pub fn is_collection(geometry_type: GeometryType) -> bool {
+    match geometry_type {
+        GeometryType::Point
+        | GeometryType::MultiPoint
+        | GeometryType::LineString
+        | GeometryType::MultiLineString
+        | GeometryType::Polygon => false,
+        GeometryType::MultiPolygon | GeometryType::GeometryCollection => true,
+        _ => panic!("is_collection: Unknown geometry type"),
+    }
+}
+
+fn visit_points<F: GeomVisitor>(
+    visitor: &mut F,
+    xy: flatbuffers::Vector<f64>,
+    offset: usize,
+    length: usize,
+) {
+    for i in (offset..offset + length).step_by(2) {
+        visitor.pointxy(xy.get(i), xy.get(i + 1));
+    }
+}
+
+pub fn visit_line<F: GeomVisitor>(
+    visitor: &mut F,
+    xy: flatbuffers::Vector<f64>,
+    offset: usize,
+    length: usize,
+) {
+    visitor.line_begin(length / 2);
+    visit_points(visitor, xy, offset, length);
+    visitor.line_end();
+}
+
+pub fn visit_multi_line<F: GeomVisitor>(visitor: &mut F, geometry: &Geometry) {
+    let xy = geometry.xy().unwrap();
+    if geometry.ends().is_none() || geometry.ends().unwrap().len() < 2 {
+        visitor.multiline_begin(1);
+        visit_line(visitor, xy, 0, xy.len());
+    } else {
+        let ends = geometry.ends().unwrap();
+        visitor.multiline_begin(ends.len() / 2);
+        let mut offset = 0;
+        for i in 0..ends.len() {
+            let end = ends.get(i) << 1;
+            visit_line(visitor, xy, offset as usize, (end - offset) as usize);
+            offset = end;
+        }
+    }
+    visitor.multiline_end();
+}
+
+pub fn visit_polygon<F: GeomVisitor>(visitor: &mut F, geometry: &Geometry) {
+    let xy = geometry.xy().unwrap();
+    if geometry.ends().is_none() || geometry.ends().unwrap().len() < 2 {
+        visitor.poly_begin(xy.len());
+        visit_points(visitor, xy, 0, xy.len());
+    } else {
+        let ends = geometry.ends().unwrap();
+        visitor.poly_begin(ends.len() / 2);
+        let mut offset = 0;
+        for i in 0..ends.len() {
+            let end = ends.get(i) << 1;
+            let length = (end - offset) as usize;
+            visitor.ring_begin(length / 2);
+            visit_points(visitor, xy, offset as usize, length);
+            visitor.ring_end();
+            offset = end;
+        }
+    }
+    visitor.poly_end();
+}
+
+pub fn visit_multi_polygon<F: GeomVisitor>(visitor: &mut F, geometry: &Geometry) {
+    let parts = geometry.parts().unwrap();
+    visitor.multipoly_begin(parts.len());
+    for i in 0..parts.len() {
+        let part = parts.get(i);
+        visit_polygon(visitor, &part);
+    }
+    visitor.multipoly_end();
+}
+
+pub fn visit_geometry<F: GeomVisitor>(
+    visitor: &mut F,
+    geometry: &Geometry,
+    geometry_type: GeometryType,
+) {
+    if !is_collection(geometry_type) {
+        let xy = geometry.xy().unwrap();
+        match geometry_type {
+            GeometryType::Point => {
+                visitor.pointxy(xy.get(0), xy.get(1));
+            }
+            GeometryType::MultiPoint => {
+                visitor.multipoint_begin(xy.len() / 2);
+                visit_points(visitor, xy, 0, xy.len());
+                visitor.multipoint_end();
+            }
+            GeometryType::LineString => {
+                visit_line(visitor, xy, 0, xy.len());
+            }
+            GeometryType::MultiLineString => {
+                visit_multi_line(visitor, geometry);
+            }
+            GeometryType::Polygon => {
+                visit_polygon(visitor, geometry);
+            }
+            _ => panic!("visit_geometry: Unknown geometry type"),
+        }
+    }
+    match geometry_type {
+        GeometryType::MultiPolygon => {
+            visit_multi_polygon(visitor, geometry);
+        }
+        _ => {} // panic!("visit_geometry: Unknown geometry type"),
     }
 }
 
--- src/rust/tests/read.rs
+++ src/rust/tests/read.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use std::io::{BufReader, Read, Seek, SeekFrom};
 
 #[test]
-fn read_file() -> std::result::Result<(), std::io::Error> {
+fn read_file_low_level() -> std::result::Result<(), std::io::Error> {
     let f = std::fs::File::open("../../test/data/countries.fgb")?;
     let mut reader = BufReader::new(f);
 
@@ -63,15 +63,23 @@ fn read_file() -> std::result::Result<(), std::io::Error> {
     let mut num_vertices = 0;
     for i in 0..parts.len() {
         let part = parts.get(i);
-        for _j in 0..part.xy().unwrap().len() {
+        for _j in (0..part.xy().unwrap().len()).step_by(2) {
             num_vertices += 1;
         }
     }
-    assert_eq!(num_vertices, 1316);
+    assert_eq!(num_vertices, 658);
 
     assert!(feature.properties().is_some());
     assert!(feature.columns().is_none());
     Ok(())
+}
+
+struct VertexCounter(u64);
+
+impl GeomVisitor for VertexCounter {
+    fn pointxy(&mut self, _x: f64, _y: f64) {
+        self.0 += 1;
+    }
 }
 
 #[test]
@@ -79,21 +87,158 @@ fn file_reader() -> std::result::Result<(), std::io::Error> {
     let f = std::fs::File::open("../../test/data/countries.fgb")?;
     let mut reader = Reader::new(f);
     let header = reader.read_header()?;
-    let cnt = header.features_count();
-    assert_eq!(cnt, 179);
-    reader.select_all()?;
-    let mut num_features = 0;
-    while let Ok(_feature) = reader.next() {
-        num_features += 1;
-    }
-    assert_eq!(cnt, num_features);
+    assert_eq!(header.geometry_type(), GeometryType::MultiPolygon);
+    assert_eq!(header.features_count(), 179);
 
+    reader.select_all()?;
+    for _ in 0..46 {
+        let _ = reader.next();
+    }
+    let feature = reader.next().unwrap();
+    // OGRFeature(countries):46
+    //   id (String) = DNK
+    //   name (String) = Denmark
+    //   MULTIPOLYGON (((12.690006 55.609991,12.089991 54.800015,11.043543 55.364864,10.903914 55.779955,12.370904 56.111407,12.690006 55.609991)),((10.912182 56.458621,1
+    // 0.667804 56.081383,10.369993 56.190007,9.649985 55.469999,9.921906 54.983104,9.282049 54.830865,8.526229 54.962744,8.120311 55.517723,8.089977 56.540012,8.256582 5
+    // 6.809969,8.543438 57.110003,9.424469 57.172066,9.775559 57.447941,10.580006 57.730017,10.546106 57.215733,10.25 56.890016,10.369993 56.609982,10.912182 56.458621))
+    // )
+    let geometry = feature.geometry().unwrap();
+
+    let mut vertex_counter = VertexCounter(0);
+    visit_geometry(&mut vertex_counter, &geometry, GeometryType::MultiPolygon);
+    assert_eq!(vertex_counter.0, 24);
+
+    Ok(())
+}
+
+#[test]
+fn magic_byte() -> std::result::Result<(), std::io::Error> {
     let f = std::fs::File::open("../../test/data/states.geojson")?;
     let mut reader = Reader::new(f);
     assert_eq!(
         reader.read_header().err().unwrap().description(),
         "Magic byte doesn\'t match"
     );
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn point_layer() -> std::result::Result<(), std::io::Error> {
+    let f = std::fs::File::open("../../test/data/ne_10m_admin_0_country_points.fgb")?;
+    let mut reader = Reader::new(f);
+    let header = reader.read_header()?;
+    assert_eq!(header.geometry_type(), GeometryType::Point);
+    assert_eq!(header.features_count(), 250);
+
+    reader.select_all()?;
+    let feature = reader.next()?;
+    assert!(feature.geometry().is_some());
+    let geometry = feature.geometry().unwrap();
+    assert_eq!(geometry.type_(), GeometryType::Unknown);
+    let xy = geometry.xy().unwrap();
+    assert_eq!(
+        (xy.get(0), xy.get(1)),
+        (2223639.4731508396, -15878634.348995442)
+    );
+
+    Ok(())
+}
+
+struct WktLineEmitter {
+    wkt: String,
+    is_first_point: bool,
+}
+
+impl GeomVisitor for WktLineEmitter {
+    fn line_begin(&mut self, _n: usize) {
+        self.wkt.push_str("LINESTRING (");
+        self.is_first_point = true;
+    }
+    fn pointxy(&mut self, x: f64, y: f64) {
+        if self.is_first_point {
+            self.is_first_point = false;
+        } else {
+            self.wkt.push_str(", ");
+        }
+        self.wkt.push_str(&format!("{} {}", x, y));
+    }
+    fn line_end(&mut self) {
+        self.wkt.push_str(")");
+    }
+}
+
+#[test]
+#[ignore]
+fn line_layer() -> std::result::Result<(), std::io::Error> {
+    let f = std::fs::File::open("../../test/data/lines.fgb")?;
+    let mut reader = Reader::new(f);
+    let header = reader.read_header()?;
+    assert_eq!(header.geometry_type(), GeometryType::LineString);
+    assert_eq!(header.features_count(), 8375);
+    reader.select_all()?;
+    let feature = reader.next()?;
+    assert!(feature.geometry().is_some());
+    let geometry = feature.geometry().unwrap();
+    assert_eq!(geometry.type_(), GeometryType::Unknown);
+    let xy = geometry.xy().unwrap();
+    let mut line = Vec::with_capacity(xy.len() / 2);
+    for i in (0..xy.len()).step_by(2) {
+        line.push((xy.get(i), xy.get(i + 1)));
+    }
+    assert_eq!(line.len(), 7);
+    assert_eq!(line[0], (1875038.4476102313, -3269648.6879248763));
+
+    let mut visitor = WktLineEmitter {
+        wkt: String::new(),
+        is_first_point: true,
+    };
+    visit_geometry(&mut visitor, &geometry, GeometryType::LineString);
+    assert_eq!(visitor.wkt, "LINESTRING (1875038.4476102313 -3269648.6879248763, 1874359.6415041967 -3270196.8129848638, 1874141.0428635243 -3270953.7840121365, 1874440.1778162003 -3271619.4315206874, 1876396.0598222911 -3274138.747656357, 1876442.0805243007 -3275052.60551469, 1874739.312657555 -3275457.333765534)");
+
+    Ok(())
+}
+
+struct MultiLineGenerator(Vec<Vec<(f64, f64)>>);
+
+impl GeomVisitor for MultiLineGenerator {
+    fn multiline_begin(&mut self, n: usize) {
+        self.0.reserve(n);
+    }
+    fn line_begin(&mut self, n: usize) {
+        self.0.push(Vec::with_capacity(n));
+    }
+    fn pointxy(&mut self, x: f64, y: f64) {
+        let len = self.0.len();
+        self.0[len - 1].push((x, y));
+    }
+}
+
+#[test]
+#[ignore]
+fn multi_line_layer() -> std::result::Result<(), std::io::Error> {
+    let f = std::fs::File::open("../../test/data/ne_10m_geographic_lines.fgb")?;
+    let mut reader = Reader::new(f);
+    let header = reader.read_header()?;
+    assert_eq!(header.geometry_type(), GeometryType::MultiLineString);
+    assert_eq!(header.features_count(), 6);
+    reader.select_all()?;
+    let feature = reader.next()?;
+    assert!(feature.geometry().is_some());
+    let geometry = feature.geometry().unwrap();
+    assert_eq!(geometry.type_(), GeometryType::Unknown);
+    let mut num_vertices = 0;
+    for _i in (0..geometry.xy().unwrap().len()).step_by(2) {
+        num_vertices += 1;
+    }
+    assert_eq!(num_vertices, 361);
+
+    let mut visitor = MultiLineGenerator(Vec::new());
+    visit_multi_line(&mut visitor, &geometry);
+    assert_eq!(visitor.0.len(), 1);
+    assert_eq!(visitor.0[0].len(), 361);
+    assert_eq!(visitor.0[0][0], (-20037505.025679983, 2692596.21474788));
 
     Ok(())
 }
