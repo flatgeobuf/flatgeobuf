3dd122af4107a7dda2128f7c3eb5d49811a62883
Björn Harrtell <bjorn@wololo.org>
2020-09-10 11:06:30 +0200

    Fix fetch

--- src/net/fetch.sh
+++ src/net/fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -L https://github.com/google/flatbuffers/archive/1.12.0.tar.gz | tar xz --strip-components=2 flatbuffers-1.12.0/net
+curl -L https://github.com/google/flatbuffers/archive/v1.12.0.tar.gz | tar xz --strip-components=2 flatbuffers-1.12.0/net
 
