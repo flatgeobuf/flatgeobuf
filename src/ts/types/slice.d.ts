64cac812250a79d3d1dc1c834fe5d027521cea3e
Björn Harrtell <bjorn@wololo.org>
2020-09-17 09:48:09 +0200

    Fix typedoc generation

--- src/ts/types/slice.d.ts
+++ src/ts/types/slice.d.ts
@@ -1,1 +1,16 @@
-declare module 'slice-source/index.js'
+declare module 'slice-source/index.js' {
+    function slice(source: ReadableStream | ReadableStreamReader | SliceSource): SliceSource;
+
+    interface SliceChunk {
+        value: Uint8Array;
+        done: boolean;
+    }
+
+    interface SliceSource {
+        slice(length: number): Promise<Uint8Array>;
+        read(): Promise<SliceChunk>;
+        cancel(): Promise<void>;
+    }
+
+    export = slice
+}
