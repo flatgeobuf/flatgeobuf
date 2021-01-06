b305e631ab85448118aac8691cb51682414e6335
Björn Harrtell <bjorn@wololo.org>
2021-01-06 14:36:52 +0100

    Forgot to commit c# deps fetcher

--- src/net/fetch.sh
+++ src/net/fetch.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-curl -L https://github.com/google/flatbuffers/archive/v1.12.0.tar.gz | tar xz --strip-components=2 flatbuffers-1.12.0/net
-
+curl -L https://github.com/google/flatbuffers/archive/v1.12.0.tar.gz | tar xz --wildcards --strip-components=2 --exclude="Properties" --directory=FlatGeobuf flatbuffers-1.12.0/net/FlatBuffers/*.cs
