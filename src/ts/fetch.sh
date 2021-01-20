ddb11e2b27b7af9cfc8845f6cab62a02bbf100ef
Björn Harrtell <bjorn@wololo.org>
2021-01-20 16:31:58 +0100

    Create dir

--- src/ts/fetch.sh
+++ src/ts/fetch.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+mkdir flatbuffers
 curl -L https://github.com/google/flatbuffers/archive/master.tar.gz | tar xz --wildcards --strip-components=2 --directory=flatbuffers flatbuffers-master/ts/*.ts
