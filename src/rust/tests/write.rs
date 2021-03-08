9105c49043877d33c9a2f187016b73af7f74e0a4
Pirmin Kalberer <pka@sourcepole.ch>
2021-03-08 23:45:07 +0100

    Use finish_size_prefixed_header_buffer

--- src/rust/tests/write.rs
+++ src/rust/tests/write.rs
@@ -1,7 +1,7 @@
 use flatgeobuf::*;
 use std::env::temp_dir;
 use std::fs::File;
-use std::io::{BufReader, Read, Write};
+use std::io::{Read, Write};
 
 #[test]
 fn verify_header() {
@@ -18,42 +18,22 @@ fn verify_header() {
         ..Default::default()
     };
     let header = Header::create(&mut builder, &header_args);
-    finish_header_buffer(&mut builder, header);
+    finish_size_prefixed_header_buffer(&mut builder, header);
     let buf = builder.finished_data();
-    let header = root_as_header(&buf).unwrap();
+    let header = size_prefixed_root_as_header(&buf).unwrap();
     assert_eq!(header.features_count(), 1);
 
     println!("{:?}", &buf);
     write_to_file(buf, &tmp_fname("header.bin"));
-    write_fgb_to_file(buf, &tmp_fname("header.fgb"));
-
-    // read into another buffer and decode again
-    let mut header_buf = Vec::with_capacity(buf.len());
-    let mut reader = BufReader::new(buf);
-    reader.read_to_end(&mut header_buf).unwrap();
-    let header = root_as_header(&header_buf).unwrap();
-    assert_eq!(header.features_count(), 1);
-
-    // Shift buffer to check alignement
-    header_buf.insert(0, 0);
-    let header = root_as_header(&header_buf[1..]).unwrap();
-    assert_eq!(header.features_count(), 1);
 
     let buf = read_from_file(&tmp_fname("header.bin"));
-    let header = root_as_header(&buf).unwrap();
+    let header = size_prefixed_root_as_header(&buf).unwrap();
     assert_eq!(header.features_count(), 1);
 
-    let buf = reader_header(&tmp_fname("header.fgb"));
-    println!("{:?}", &buf);
-    // [28, 0, 0, 0, 0, 0, 22, 0, 32, 0, 8, 0, 12, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 0, 20, 0, 22, 0, 0, 0, 0, 0, 0, 17, 64, 0, 0, 0, 20, 0, 0, 0, 72, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 34, 64, 0, 0, 0, 0, 0, 0, 34, 64, 0, 0, 0, 0, 8, 0, 0, 0, 116, 114, 105, 97, 110, 103, 108, 101, 0, 0, 0, 0, 0, 0, 0, 0]
-    let header = root_as_header(&buf).unwrap();
-    assert_eq!(header.features_count(), 1);
-
-    let buf = read_from_file(&tmp_fname("header.fgb"));
-    let header = root_as_header(&buf[12..]).unwrap();
-    assert_eq!(header.features_count(), 1);
-
-    assert!(size_prefixed_root_as_header(&buf[8..]).is_err(), "Verfication with size prefix fails");
+    assert!(
+        root_as_header(&buf[4..]).is_err(),
+        "Verfication without size prefix fails"
+    );
 }
 
 fn tmp_fname(fname: &str) -> String {
@@ -67,37 +47,11 @@ fn write_to_file(data: &[u8], fname: &str) {
     file.write_all(data).expect("write failed");
 }
 
-fn write_fgb_to_file(data: &[u8], fname: &str) {
-    let mut file = File::create(fname).expect("create failed");
-    file.write_all(&MAGIC_BYTES).expect("write failed");
-    file.write_all(&(data.len() as u32).to_le_bytes())
-        .expect("write failed");
-    file.write_all(data).expect("write failed");
-}
-
 fn read_from_file(fname: &str) -> Vec<u8> {
     let mut f = File::open(fname).expect("open failed");
     let mut buf = Vec::new();
     f.read_to_end(&mut buf).expect("read failed");
     buf
-}
-
-fn reader_header(fname: &str) -> Vec<u8> {
-    let f = File::open(fname).expect("open failed");
-    let mut reader = BufReader::new(f);
-
-    let mut magic_buf: [u8; 8] = [0; 8];
-    reader.read_exact(&mut magic_buf).expect("read failed");
-    // assert_eq!(magic_buf, MAGIC_BYTES);
-
-    let mut size_buf: [u8; 4] = [0; 4];
-    reader.read_exact(&mut size_buf).expect("read failed");
-    let header_size = u32::from_le_bytes(size_buf);
-
-    let mut header_buf = vec![0; header_size as usize];
-    reader.read_exact(&mut header_buf).expect("read failed");
-
-    header_buf
 }
 
 #[test]
@@ -154,5 +108,8 @@ fn header_fgb_only() {
     println!("{:?}", &header);
     assert_eq!(header.features_count(), 1);
 
-    assert!(root_as_header(&buf[4..]).is_err(), "Verfication without size prefix fails");
+    assert!(
+        root_as_header(&buf[4..]).is_err(),
+        "Verfication without size prefix fails"
+    );
 }
