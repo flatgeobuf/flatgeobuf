79ba5b8c73a349509e9d579a3db25e770814cbaa
Björn Harrtell <bjorn@wololo.org>
2021-03-08 23:27:24 +0100

    Add verifier cases for header buffer

--- src/cpp/test/header.h
+++ src/cpp/test/header.h
@@ -19,8 +19,17 @@ TEST_CASE("Header")
         auto header = CreateHeaderDirect(fbb, "Test", envelope, GeometryType::Point, false, false, false, false, columns, features_count);
 
         fbb.FinishSizePrefixed(header);
-        // uint8_t *buf = fbb.GetBufferPointer();
+        uint8_t *buf = fbb.GetBufferPointer();
         int size = fbb.GetSize();
+
+        Verifier v1(buf, size);
+        Verifier v2(buf, size);
+        Verifier v3(buf + 4, size - 4);
+        Verifier v4(buf + 24, size - 24); // intentional move into mid buffer
+        REQUIRE(VerifyHeaderBuffer(v1) == true);
+        REQUIRE(VerifySizePrefixedHeaderBuffer(v2) == true);
+        REQUIRE(VerifyHeaderBuffer(v3) == true);
+        REQUIRE(VerifyHeaderBuffer(v4) == false);
 
         REQUIRE(size == 44);
     }
