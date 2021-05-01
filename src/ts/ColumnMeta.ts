c57150181ea148199fbeba131d0bb7f40cc5a588
Björn Harrtell <bjorn@wololo.org>
2021-05-01 15:29:06 +0200

    Remove unused arrayType

--- src/ts/ColumnMeta.ts
+++ src/ts/ColumnMeta.ts
@@ -1,26 +1,7 @@
 
 import { ColumnType } from './header_generated'
 
-const arrayTypeMap = {
-    [ColumnType.Byte]: Uint8Array,
-    [ColumnType.UByte]: Uint8Array,
-    [ColumnType.Bool]: Uint8Array,
-    [ColumnType.Short]: Uint16Array,
-    [ColumnType.UShort]: Uint16Array,
-    [ColumnType.Int]: Uint32Array,
-    [ColumnType.UInt]: Uint32Array,
-    [ColumnType.Long]: BigUint64Array,
-    [ColumnType.ULong]: BigUint64Array,
-    [ColumnType.Float]: Float64Array,
-    [ColumnType.Double]: Float64Array,
-    [ColumnType.String]: String,
-    [ColumnType.Json]: String,
-    [ColumnType.DateTime]: String,
-    [ColumnType.Binary]: String
-}
-
 export default class ColumnMeta {
-    arrayType: any
     constructor(
         public name: string,
         public type: ColumnType,
@@ -33,6 +14,5 @@ export default class ColumnMeta {
         public unique: boolean,
         public primary_key: boolean
         ) {
-        this.arrayType = arrayTypeMap[type]
     }
 }
