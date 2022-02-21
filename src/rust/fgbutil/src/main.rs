1bdff118229cf1ef017e2346a88ab2ac5806f9e8
Björn Harrtell <bjorn@wololo.org>
2022-02-21 19:05:56 +0100

    Attempt to allow different output formats

--- src/rust/fgbutil/src/main.rs
+++ src/rust/fgbutil/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::Stdout;
+use std::io::Write;
+use flatgeobuf::FgbWriter;
 use geozero::GeozeroDatasource;
 use flatgeobuf::FgbReader;
 use std::fs::File;
@@ -34,13 +37,26 @@ enum Format {
     Geojson,
 }
 
+enum FormatProcessor<'a, W: Write> {
+    Geojson(GeoJsonWriter<'a, W>),
+    Flatgeobuf(FgbWriter<'a>),
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
     let mut filein = BufReader::new(File::open(args.input)?);
     let mut reader = FgbReader::open(&mut filein)?;
+    let name = reader.header().name().unwrap();
+    let geometry_type = reader.header().geometry_type();
     let mut output = std::io::stdout();
-    let mut writer = GeoJsonWriter::new(&mut output);
+    let processor: FormatProcessor<Stdout> = match args.inputformat {
+        Format::Geojson => FormatProcessor::Geojson(GeoJsonWriter::new(&mut output)),
+        Format::Flatgeobuf => FormatProcessor::Flatgeobuf(FgbWriter::create(name, geometry_type, |_, _| {})?),
+    };
     reader.select_all()?;
-    reader.process(&mut writer)?;
+    match processor {
+        FormatProcessor::Geojson(mut value) => reader.process(&mut value)?,
+        FormatProcessor::Flatgeobuf(mut value) => reader.process(&mut value)?
+    }
     Ok(())
 }
