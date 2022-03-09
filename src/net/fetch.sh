f24f87a58e54baa471dfec343747871aaaa99f43
Björn Harrtell <bjorn@wololo.org>
2022-03-09 16:52:39 +0100

    C# Upgrade Flatbuffers

--- src/net/fetch.sh
+++ src/net/fetch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -L https://github.com/google/flatbuffers/archive/v1.12.0.tar.gz | tar xz --wildcards --strip-components=2 --exclude="Properties" --directory=FlatGeobuf flatbuffers-1.12.0/net/FlatBuffers/*.cs
+curl -L https://github.com/google/flatbuffers/archive/v2.0.6.tar.gz | tar xz --wildcards --strip-components=2 --exclude="Properties" --directory=FlatGeobuf flatbuffers-2.0.6/net/FlatBuffers/*.cs
