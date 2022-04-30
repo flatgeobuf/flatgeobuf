b4c180a7566a29783c42da99bc7b88d73dfef5d6
Pirmin Kalberer <pka@sourcepole.ch>
2022-04-30 10:53:17 +0200

    Basic info command for fgbutil

--- src/rust/fgbutil/src/main.rs
+++ src/rust/fgbutil/src/main.rs
@@ -1,5 +1,5 @@
-use clap::{ArgEnum, Parser};
-use flatgeobuf::{FgbReader, FgbWriter};
+use clap::{ArgEnum, Args, Parser, Subcommand};
+use flatgeobuf::{FallibleStreamingIterator, FgbReader, FgbWriter};
 use geozero::error::Result;
 use geozero::geojson::{GeoJsonReader, GeoJsonWriter};
 use geozero::GeozeroDatasource;
@@ -8,7 +8,22 @@ use std::io::{BufRead, BufReader, BufWriter, Write};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
-struct Args {
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Convert a file
+    Convert(Convert),
+    /// Info about a FlatGeobuf file
+    Info(Info),
+}
+
+#[derive(Args)]
+struct Convert {
     /// Input path
     #[clap(short, long)]
     input: Option<String>,
@@ -28,6 +43,21 @@ struct Args {
     /// Make output indexed
     #[clap(long)]
     index: bool,
+}
+
+#[derive(Args)]
+struct Info {
+    /// Input path
+    #[clap(short, long)]
+    input: Option<String>,
+
+    /// Dump index
+    #[clap(long)]
+    index: bool,
+
+    /// Dump n-th feature
+    #[clap(long)]
+    dump: Option<usize>,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
@@ -92,22 +122,61 @@ fn transform(
     Ok(())
 }
 
+fn info(mut input: impl BufRead, args: &Info) -> Result<()> {
+    let mut fgb = FgbReader::open(&mut input)?;
+
+    println!("{:#?}", &fgb.header());
+
+    if args.index {
+        fgb.process_index(&mut GeoJsonWriter::new(&mut std::io::stdout()))?;
+        println!();
+    }
+
+    if let Some(fno) = args.dump {
+        if args.index {
+            println!("Can't dump feature together with index - skipping");
+            return Ok(());
+        }
+        let mut fgb = fgb.select_all_seq()?;
+        let mut n = 0;
+        while let Some(feature) = fgb.next()? {
+            if n == fno {
+                println!("{:#?}", &feature.fbs_feature());
+                break;
+            }
+            n += 1;
+        }
+    }
+    Ok(())
+}
+
 fn main() -> Result<()> {
-    let args = Args::parse();
-    let input: Box<dyn BufRead> = match args.input {
-        Some(x) => Box::new(BufReader::new(File::open(x)?)),
-        None => Box::new(BufReader::new(std::io::stdin())),
-    };
-    let output: Box<dyn Write> = match args.output {
-        Some(x) => Box::new(BufWriter::new(File::create(x)?)),
-        None => Box::new(BufWriter::new(std::io::stdout())),
-    };
-    transform(
-        args.inputformat,
-        args.outputformat,
-        input,
-        output,
-        args.index,
-    )?;
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Convert(args) => {
+            let input: Box<dyn BufRead> = match &args.input {
+                Some(x) => Box::new(BufReader::new(File::open(x)?)),
+                None => Box::new(BufReader::new(std::io::stdin())),
+            };
+            let output: Box<dyn Write> = match &args.output {
+                Some(x) => Box::new(BufWriter::new(File::create(x)?)),
+                None => Box::new(BufWriter::new(std::io::stdout())),
+            };
+            transform(
+                args.inputformat,
+                args.outputformat,
+                input,
+                output,
+                args.index,
+            )?;
+        }
+        Commands::Info(args) => {
+            let input: Box<dyn BufRead> = match &args.input {
+                Some(x) => Box::new(BufReader::new(File::open(x)?)),
+                None => Box::new(BufReader::new(std::io::stdin())),
+            };
+            info(input, args)?;
+        }
+    }
     Ok(())
 }
