1552b6f9250fc08b00b4da2efbfc6b935372e00e
Björn Harrtell <bjorn@wololo.org>
2023-02-03 19:53:11 +0100

    NET: Ignore warning due to conditional compilation

--- src/net/FlatGeobuf/NTS/AsyncFeatureEnumerator.cs
+++ src/net/FlatGeobuf/NTS/AsyncFeatureEnumerator.cs
@@ -131,6 +131,7 @@ namespace FlatGeobuf.NTS
         public IFeature Current { get; private set; }
 
         /// <inheritdoc/>
+        #pragma warning disable CS1998
         public async ValueTask DisposeAsync()
         {
             _itemEnumerator?.Dispose();
@@ -140,6 +141,7 @@ namespace FlatGeobuf.NTS
             _stream.Dispose();
 #endif
         }
+        #pragma warning restore CS1998
 
         /// <inheritdoc/>
         public async ValueTask<bool> MoveNextAsync()
