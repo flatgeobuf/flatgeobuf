fa8f33502e8a88a380b2249b247aa75f44300d84
Björn Harrtell <bjorn@wololo.org>
2023-03-08 17:32:09 +0100

    Upgrade to flatbuffers 23.3.3

--- src/net/FlatGeobuf/Feature.cs
+++ src/net/FlatGeobuf/Feature.cs
@@ -13,7 +13,7 @@ public struct Feature : IFlatbufferObject
 {
   private Table __p;
   public ByteBuffer ByteBuffer { get { return __p.bb; } }
-  public static void ValidateVersion() { FlatBufferConstants.FLATBUFFERS_23_1_21(); }
+  public static void ValidateVersion() { FlatBufferConstants.FLATBUFFERS_23_3_3(); }
   public static Feature GetRootAsFeature(ByteBuffer _bb) { return GetRootAsFeature(_bb, new Feature()); }
   public static Feature GetRootAsFeature(ByteBuffer _bb, Feature obj) { return (obj.__assign(_bb.GetInt(_bb.Position) + _bb.Position, _bb)); }
   public void __init(int _i, ByteBuffer _bb) { __p = new Table(_i, _bb); }
--- src/net/fetch.sh
+++ src/net/fetch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -L https://github.com/google/flatbuffers/archive/v23.1.21.tar.gz | tar xz --wildcards --strip-components=2 --exclude="Properties" --directory=FlatGeobuf flatbuffers-23.1.21/net/FlatBuffers/*.cs
+curl -L https://github.com/google/flatbuffers/archive/v23.3.3.tar.gz | tar xz --wildcards --strip-components=2 --exclude="Properties" --directory=FlatGeobuf flatbuffers-23.1.21/net/FlatBuffers/*.cs
