3c5dc79d64c061597889713a01c4683ba3f369c9
Björn Harrtell <bjorn@wololo.org>
2023-07-19 20:17:51 +0200

    Remove unused import

--- src/java/src/main/java/org/wololo/flatgeobuf/PackedRTree.java
+++ src/java/src/main/java/org/wololo/flatgeobuf/PackedRTree.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
