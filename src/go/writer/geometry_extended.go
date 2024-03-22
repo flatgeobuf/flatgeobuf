b56461b7e4be783be80ce81acf0738fac92b0965
Adrien Durand <adrien.943@gmail.com>
2024-03-22 17:39:51 +0100

    [go] fix geometry parts build

--- src/go/writer/geometry_extended.go
+++ src/go/writer/geometry_extended.go
@@ -13,6 +13,32 @@ func NewGeometryExtended(geometry *Geometry) *GeometryExtended {
 }
 
 func (g *GeometryExtended) BoundingBox() (minX, minY, maxX, maxY float64) {
+	if len(g.parts) == 0 {
+		return geomBoundingBox(g.Geometry)
+	}
+	minX = math.Inf(1)
+	minY = math.Inf(1)
+	maxX = math.Inf(-1)
+	maxY = math.Inf(-1)
+	for i := 0; i < len(g.parts); i++ {
+		partMinX, partMinY, partMaxX, partMaxY := geomBoundingBox(&g.parts[i])
+		if partMinX < minX {
+			minX = partMinX
+		}
+		if partMinY < minY {
+			minY = partMinY
+		}
+		if partMaxX > maxX {
+			maxX = partMaxX
+		}
+		if partMaxY > maxY {
+			maxY = partMaxY
+		}
+	}
+	return
+}
+
+func geomBoundingBox(g *Geometry) (minX, minY, maxX, maxY float64) {
 	firstPartEnd := len(g.xy)
 	if firstPartEnd == 0 {
 		// Nothing to do.
@@ -20,7 +46,7 @@ func (g *GeometryExtended) BoundingBox() (minX, minY, maxX, maxY float64) {
 	}
 
 	if len(g.ends) != 0 {
-		firstPartEnd = int(g.ends[0])
+		firstPartEnd = int(g.ends[0]) * 2
 	}
 	firstPart := g.xy[0:firstPartEnd]
 
