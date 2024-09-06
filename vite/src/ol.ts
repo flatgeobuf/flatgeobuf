d1a2fbb63507bcd6fc1a983d1a17dd60fb8717aa
Björn Harrtell <bjorn@septima.dk>
2024-09-06 11:00:22 +0200

    Basic vite openlayers site

--- vite/src/ol.ts
+++ vite/src/ol.ts
@@ -1,1 +1,18 @@
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
 
+new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  controls: [],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
