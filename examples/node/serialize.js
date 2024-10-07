d9b91f4f7edf88476bf4e1dfef73effaf69e4787
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2024-10-07 14:14:03 +0200

    Update serialize.js

--- examples/node/serialize.js
+++ examples/node/serialize.js
@@ -7,6 +7,7 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 const data = readFileSync(path.join(__dirname, '../../test/data/countries.geojson'))
 const jsonData = JSON.parse(data)
+// NOTE: be aware that TS/JS implementation creates files with no spatial index
 const serializedFeatures = serialize(jsonData)
 
 const reDeserializedData = deserialize(new Uint8Array(serializedFeatures))
