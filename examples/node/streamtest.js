c0957e28f345a34787127f37a72b7119a949529c
Björn Harrtell <bjorn@septima.dk>
2024-12-23 12:43:26 +0100

    [ts] Refactor away use of stream-buffers

--- examples/node/streamtest.js
+++ examples/node/streamtest.js
@@ -1,57 +1,8 @@
 import { geojson } from 'flatgeobuf';
 
-function nodeToWeb(nodeStream) {
-    let destroyed = false;
-    const listeners = {};
-
-    function start(controller) {
-        listeners.data = onData;
-        listeners.end = onData;
-        listeners.end = onDestroy;
-        listeners.close = onDestroy;
-        listeners.error = onDestroy;
-        for (const name in listeners) nodeStream.on(name, listeners[name]);
-
-        nodeStream.pause();
-
-        function onData(chunk) {
-            if (destroyed) return;
-            controller.enqueue(chunk);
-            nodeStream.pause();
-        }
-
-        function onDestroy(err) {
-            if (destroyed) return;
-            destroyed = true;
-
-            for (const name in listeners) nodeStream.removeListener(name, listeners[name]);
-
-            if (err) controller.error(err);
-            else controller.close();
-        }
-    }
-
-    function pull() {
-        if (destroyed) return;
-        nodeStream.resume();
-    }
-
-    function cancel() {
-        destroyed = true;
-
-        for (const name in listeners) nodeStream.removeListener(name, listeners[name]);
-
-        nodeStream.push(null);
-        nodeStream.pause();
-        nodeStream.destroy();
-    }
-
-    return new ReadableStream({ start: start, pull: pull, cancel: cancel });
-}
-
 async function streamtest() {
     const response = await fetch('https://flatgeobuf.org/test/data/UScounties.fgb');
-    for await (const feature of geojson.deserialize(nodeToWeb(response.body)))
+    for await (const feature of geojson.deserialize(response.body))
         console.log(JSON.stringify(feature, undefined, 1));
 }
 
--- src/ts/streams/utils.ts
+++ src/ts/streams/utils.ts
@@ -1,18 +1,8 @@
-import type { Readable } from 'node:stream';
-import { ReadableStreamBuffer } from 'stream-buffers';
+import { Readable } from 'node:stream';
 
 export function arrayToStream(array: ArrayBuffer): ReadableStream {
-    const myReadableStreamBuffer = new ReadableStreamBuffer({
-        frequency: 10, // in milliseconds.
-        chunkSize: 2048, // in bytes.
-    });
-
-    myReadableStreamBuffer.put(Buffer.from(array));
-    myReadableStreamBuffer.stop();
-
-    const webReader = nodeToWeb(myReadableStreamBuffer);
-
-    return webReader;
+    const nodeStream = Readable.from(Buffer.from(array));
+    return Readable.toWeb(nodeStream) as ReadableStream;
 }
 
 export async function takeAsync<T>(asyncIterable: AsyncIterable<T>, count = Number.POSITIVE_INFINITY): Promise<T[]> {
@@ -24,54 +14,4 @@ export async function takeAsync<T>(asyncIterable: AsyncIterable<T>, count = Numb
         result.push(value);
     }
     return result;
-}
-
-export function nodeToWeb(nodeStream: Readable): ReadableStream {
-    let destroyed = false;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const listeners: { [key: string]: (chunk: Buffer | Error) => void } = {};
-
-    function start(controller: ReadableStreamDefaultController) {
-        listeners.data = onData as (chunk: Buffer | Error) => void;
-        listeners.end = onData as (chunk: Buffer | Error) => void;
-        listeners.end = onDestroy as (chunk: Buffer | Error) => void;
-        listeners.close = onDestroy as (chunk: Buffer | Error) => void;
-        listeners.error = onDestroy as (chunk: Buffer | Error) => void;
-        for (const name in listeners) nodeStream.on(name, listeners[name]);
-
-        nodeStream.pause();
-
-        function onData(chunk: Buffer) {
-            if (destroyed) return;
-            controller.enqueue(chunk);
-            nodeStream.pause();
-        }
-
-        function onDestroy(err: Error) {
-            if (destroyed) return;
-            destroyed = true;
-
-            for (const name in listeners) nodeStream.removeListener(name, listeners[name]);
-
-            if (err) controller.error(err);
-            else controller.close();
-        }
-    }
-
-    function pull() {
-        if (destroyed) return;
-        nodeStream.resume();
-    }
-
-    function cancel() {
-        destroyed = true;
-
-        for (const name in listeners) nodeStream.removeListener(name, listeners[name]);
-
-        nodeStream.push(null);
-        nodeStream.pause();
-        nodeStream.destroy();
-    }
-
-    return new ReadableStream({ start: start, pull: pull, cancel: cancel });
 }
