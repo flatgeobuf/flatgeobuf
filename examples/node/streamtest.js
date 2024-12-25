24e9acf148acb869f9eee635eb16e26530ec3c5a
Björn Harrtell <bjorn@septima.dk>
2024-12-25 03:22:31 +0100

    [ts] Fix lint errors

--- examples/node/streamtest.js
+++ examples/node/streamtest.js
@@ -2,8 +2,7 @@ import { geojson } from 'flatgeobuf';
 
 async function streamtest() {
     const response = await fetch('https://flatgeobuf.org/test/data/UScounties.fgb');
-    for await (const feature of geojson.deserialize(response.body))
-        console.log(JSON.stringify(feature, undefined, 1));
+    for await (const feature of geojson.deserialize(response.body)) console.log(JSON.stringify(feature, undefined, 1));
 }
 
 streamtest();
